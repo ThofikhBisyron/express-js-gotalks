5af76f7f969c62aeffd4c52a282cfcc1a22e62e8
ThofikhBisyron <sanada.ryuzaki1@gmail.com>
2025-05-16 05:41:15 +0700

    feat: add func mark as read in model and controller

--- controllers/chat.js
+++ controllers/chat.js
@@ -1,4 +1,4 @@
-const {createMessage} = require("../models/chat")
+const {createMessage, markMessageAsRead} = require("../models/chat")
 
 const sendMessage = async (req, res) => {
     const {receiverId, groupId, content} = req.body
@@ -16,6 +16,21 @@ const sendMessage = async (req, res) => {
     } catch(err) {
         console.log(err)
         return res.status(500).json({message: "An error occurred on the server"})
+    }
+}
+
+const markAsRead = async (req, res) => {
+    const {messageId} = req.params
+    const userId = req.user.id
+
+    try{
+        await markMessageAsRead(messageId, userId)
+        res.status(200).json({message: "Message marked as read"})
+    }
+
+     catch(err) {
+        console.log(err)
+        res.status(500).json({message: "An error occurred on the server"})
     }
 }
 
