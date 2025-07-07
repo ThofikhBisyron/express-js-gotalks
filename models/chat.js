440b455cc68fe61f1538b4b7843cc90e5484e8b2
ThofikhBisyron <sanada.ryuzaki1@gmail.com>
2025-07-07 07:24:03 +0700

    feat: add models for chatlist

--- models/chat.js
+++ models/chat.js
@@ -3,7 +3,31 @@ const pool = require("../config/db")
 const createMessage = async (senderId, receiverId, groupID, content) => {
     const query = `INSERT INTO messages (sender_id, receiver_id, group_id, content) VALUES ($1, $2, $3, $4) RETURNING *`
     const result = await pool.query(query, [senderId, receiverId, groupID, content])
-    return result.rows[0]
+    const message = result.rows[0]
+
+    if (groupID) {
+        await pool.query(
+            `INSERT INTO chat_list (user_id, target_id, type, last_message_id, updated_at)
+            VALUES($1, $2, 'group', $3, NOW())
+            ON CONFLICT (user_id, target_id, 'type')
+            DO UPDATE SET last_message_id = $3, updated_at = NOW()`, [senderId, groupID, message.id]
+        )
+    } else {
+        await pool.query(
+            `INSERT INTO chat_list (user_id, target_id, type, last_message_id, updated_at)
+            VALUES ($1, $2, 'user', $3, NOW())
+            ON CONFLICT (user_id, target_id, type)
+            DO UPDATE SET last_message_id = $3, updated_at = NOW()`, [senderId, receiverId, message.id]
+        )
+
+        await pool.query(
+            `INSERT INTO chat_list (user_id, target_id, type, last_message_id, updated_at)
+            VALUES $1, $2, 'user', $3, NOW()
+            ON CONFLICT (user_id, target_id, type)
+            DO UPDATE SET last_message_id = $3, updated_at = NOW()`, [receiverId, senderId, message.id]
+        )
+    }
+    return message
 }
 
 const markMessageAsRead = async (messageId, userId) => {
