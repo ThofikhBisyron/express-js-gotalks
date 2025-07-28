61f59da99aa3bf1f2ce12d8dd581bdf7cdf694ea
ThofikhBisyron <sanada.ryuzaki1@gmail.com>
2025-07-29 00:22:27 +0700

    feat: add chatlist routes

--- routes/chat.js
+++ routes/chat.js
@@ -1,12 +1,13 @@
 const express = require("express")
 const {authenticateToken, requireVerifiedUser} = require("../middlewares/authMiddleware")
-const {sendMessage, markAsRead, getMessages, getReadStatus, } = require("../controllers/chat")
+const {sendMessage, markAsRead, getMessages, getReadStatus, getChatListbyId } = require("../controllers/chat")
 const router = express.Router()
 
 router.post("/send", authenticateToken, requireVerifiedUser, sendMessage)
 router.post("/read/:messageId", authenticateToken, requireVerifiedUser, markAsRead)
 router.get("/messages", authenticateToken, requireVerifiedUser, getMessages)
 router.get("/status/:messageId", authenticateToken, requireVerifiedUser, getReadStatus)
+router.get("/chatlist", authenticateToken, requireVerifiedUser, getChatListbyId)
 
 module.exports = router
 
