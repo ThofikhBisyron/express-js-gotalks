f01fec80ba61e905e7fe1537e2cdc4a1711f97ae
ThofikhBisyron <sanada.ryuzaki1@gmail.com>
2025-08-13 02:07:47 +0700

    feat: add user image and background routes

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,16 @@
 const express = require('express');
-const { registerOrLogin, verifyOtp, updateUsername } = require('../controllers/user');
+const { registerOrLogin, verifyOtp, updateUsername, getUser, updatedImageUser, updatedImgBgUser } = require('../controllers/user');
 const { authenticateToken, requireVerifiedUser } = require('../middlewares/authMiddleware')
-
+const createUploader = require('../middlewares/upload')
+const updatedProfileImage = createUploader("profile")
+const updatedBgProfile = createUploader("profile_background")
 const router = express.Router();
 
 router.post('/register', registerOrLogin);
 router.post('/verify-otp', verifyOtp)
 router.patch('/update-username', authenticateToken, requireVerifiedUser, updateUsername)
+router.get('/profile', authenticateToken, requireVerifiedUser, getUser)
+router.patch('/profileimage', authenticateToken, requireVerifiedUser, updatedProfileImage.single("image"), updatedImageUser)
+router.patch('/bgprofile', authenticateToken, requireVerifiedUser, updatedBgProfile.single("imgBg"), updatedImgBgUser)
 
 module.exports = router;
