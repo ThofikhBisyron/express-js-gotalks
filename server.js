f1255c20ef347b5c9d554c345e792875e8de8217
ThofikhBisyron <sanada.ryuzaki1@gmail.com>
2025-11-10 00:08:39 +0700

    feat: add socket in express

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 require('dotenv').config();
+const http = require('http')
+const {Server} = require("socket.io")
 const app = require('./app');
+const chatSocket = require('./socket/chatSocket');
 const PORT = process.env.PORT 
 
+const server = http.createServer(app)
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+}) 
 
-app.listen(PORT, '0.0.0.0', () => {
+
+chatSocket(io)
+
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
---
+++ socket/chatSocket.js
@@ -0,0 +1,59 @@
+const { createMessage, getMessagesId, getChatList} = require("../models/chat")
+const { authenticateSocket } = require("../middlewares/authMiddleware")
+
+function chatSocket(io) {
+    io.use(authenticateSocket)
+
+    io.on('connection', (socket) =>{
+        const userId = socket.user.id
+        console.log(`User ${userId} connected`)
+
+        socket.join(`user_${userId}`)
+
+        socket.on('join_chat', (data) => {
+            const {groupId} = data
+            if (groupId) {
+                socket.join(`group_${groupId}`)
+                console.log(`User ${userId} joined group_${groupId}`)
+            }
+        })
+
+        socket.on(`send_message`, async (data) =>{
+            const {receiverId, groupId, content} = data
+
+            try{
+                const message = await createMessage(userId, receiverId, groupId, content)
+                
+                socket.emit('message_sent', message)
+
+                if (groupId) {
+                    io.to(`group_${groupId}`).emit(`new_message`, message)
+                } else {
+                    io.to(`user_${receiverId}`).emit(`new_message`, message)
+                }
+
+            }catch(err){
+                console.error(`Error sending message:`, err)
+                socket.emit(`error_message`, {message : `failed to sent message`})
+            }
+        })
+        
+        socket.on(`get_message`, async (data) => {
+            const {receiverId, groupId} = data
+            const message = await getMessagesId(userId, receiverId, groupId)
+            socket.emit(`message_history`, message)
+        })
+
+        socket.on(`get_chat_list`, async () => {
+            const chatList = await getChatList(userId)
+            socket.emit(`chat_list`, chatList)
+        })
+
+        socket.on(`disconnect`, () =>{
+            console.log(`User ${userId} disconnect`)
+        })
+
+    })
+} 
+
+module.exports = chatSocket
