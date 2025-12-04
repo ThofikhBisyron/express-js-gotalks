da1c2984828c8a439aefde74fe49bad37f247d80
ThofikhBisyron <sanada.ryuzaki1@gmail.com>
2025-12-04 11:53:09 +0700

    change db

--- config/db.js
+++ config/db.js
@@ -3,11 +3,16 @@ require('dotenv').config();
 
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
+    host: "postgres",
+    port: 5432,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
 });
 
 module.exports = pool;
+
+//  host: process.env.DB_HOST,
+//  port: Number(process.env.DB_PORT),
+// host: "postgres",
+// port: 5432,
