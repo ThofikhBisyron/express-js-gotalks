40631dd94c389118e0f34af218fee4fe3c362f6f
ThofikhBisyron <sanada.ryuzaki1@gmail.com>
2026-02-07 08:32:27 +0700

    fix error

--- models/fcmPush.js
+++ models/fcmPush.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const pool = require("../config/db");
 
 const savePushTokenFCM = async (userId, token, deviceType = 'android') => {
@@ -24,30 +23,4 @@ const getPushTokensByUserIdFCM = async (userId) => {
 };
 
 module.exports = { savePushTokenFCM, getPushTokensByUserIdFCM };
-=======
-const pool = require("../config/db");
 
-const savePushTokenFCM = async (userId, token, deviceType = 'android') => {
-  await pool.query(
-    `
-    INSERT INTO user_fcm_tokens (user_id, expo_fcm_token, device_type)
-    VALUES ($1, $2, $3)
-    ON CONFLICT (user_id, device_type)
-    DO UPDATE SET
-      expo_fcm_token = EXCLUDED.expo_fcm_token,
-      updated_at = NOW()
-    `,
-    [userId, token, deviceType]
-  );
-};
-
-const getPushTokensByUserIdFCM = async (userId) => {
-  const result = await pool.query(
-    `SELECT expo_fcm_token FROM user_fcm_tokens WHERE user_id = $1`,
-    [userId]
-  );
-  return result.rows.map(r => r.expo_fcm_token)
-};
-
-module.exports = { savePushTokenFCM, getPushTokensByUserIdFCM };
->>>>>>> 730e1481799a6bbeeaeb0fc7484ee05bdc00e61d
